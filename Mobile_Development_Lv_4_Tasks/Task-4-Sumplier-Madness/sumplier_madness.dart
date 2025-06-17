1c1a940358ac763bb42cb9db8cf1439f794aae54
AsherWood39 <athishtapa05@gmail.com>
2025-06-17 18:02:07 +0530

    Updates to the repo

--- Mobile_Development_Lv_4_Tasks/Task-4-Sumplier-Madness/sumplier_madness.dart
+++ Mobile_Development_Lv_4_Tasks/Task-4-Sumplier-Madness/sumplier_madness.dart
@@ -7,7 +7,7 @@ void main() {
   var num = int.parse(stdin.readLineSync()!);
   int sum = 0;
 
-  print('\n The Multiplication Table of $num is : ');
+  print('\n The Multiplication Table of $num is : \n');
   for (int i = 1; i <= 10; i++) {
     print("\t|\t$i * $num = ${num * i}\t|  ");
     sum += num * i;
